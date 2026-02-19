8464fce2f9da0e4eb36017959cbc9dcdcf4d5d0c
dhanush <dhanush.eashwar@berkeley.edu>
2026-02-19 15:26:33 -0800

    added mobile project

---
+++ Actionable/Actionable/ActionableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionableApp.swift
+//  Actionable
+//
+//  Created by Dhanush Eashwar on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ActionableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
